a02c8da72102038c751033b227395188361d1974
Cao Tong <tony.caotong@gmail.com>
2017-02-13 17:49:29 +0800

    Add decoder.

---
+++ decoder.c
@@ -0,0 +1,11 @@
+/**
+ * 	Masked potato salad.
+ *		by Cao tong <tony_caotong@gmail.com>
+ *		@ 2017-02-10
+ *
+ */
+
+#include "decoder.h"
+
+
+
---
+++ decoder.h
@@ -0,0 +1,11 @@
+/**
+ * 	Masked potato salad.
+ *		by Cao tong <tony_caotong@gmail.com>
+ *		@ 2017-02-10
+ *
+ */
+
+#ifndef __DECODER_H__
+#define __DECODER_H__
+
+#endif /*__DECODER_H__*/
